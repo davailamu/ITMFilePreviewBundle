970d0ef9278fd32589ed79355a536e802f02e8e9
Alexei Yanochkin <alexei.yanochkin@itmhouse.com>
2014-09-09 10:24:23 +0300

    Init File preview bundle

---
+++ ITMFilePreviewBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ITM\FilePreviewBundle;
+
+use ITM\Sonata\ImagePreviewBundle\DependencyInjection\ImagePreviewExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ITMFilePreviewBundle extends Bundle
+{
+}
