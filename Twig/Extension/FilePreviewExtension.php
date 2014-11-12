c10e81c9983b9a993cc624b07c11129a5eb7f577
Alexei Yanochkin <alexei.yanochkin@itmhouse.com>
2014-11-12 15:52:11 +0300

    Убрали зависимость от filesize

--- Twig/Extension/FilePreviewExtension.php
+++ Twig/Extension/FilePreviewExtension.php
@@ -29,25 +29,67 @@ class FilePreviewExtension extends \Twig_Extension
             new \Twig_SimpleFilter('itm_file_url', array($this, 'resolveUrl')),
             new \Twig_SimpleFilter('itm_file_path', array($this, 'resolvePath')),
             new \Twig_SimpleFilter('itm_file_exists', array($this, 'fileExists')),
+            new \Twig_SimpleFilter('readable_filesize', array($this, 'readableFilesize')),
         );
     }
 
-
+    /**
+     * Url к файлу
+     * @param $entity
+     * @param $field
+     * @return string
+     */
     public static function resolveUrl( $entity, $field )
     {
         return self::$pathResolver->getUrl($entity, $field);
     }
 
+    /**
+     * Путь к файлу
+     * @param $entity
+     * @param $field
+     * @return string
+     */
     public static function resolvePath( $entity, $field )
     {
         return self::$pathResolver->getPath($entity, $field, true);
     }
 
+    /**
+     * Проверка на существование файла
+     * @param $entity
+     * @param $field
+     * @return bool
+     */
     public static function fileExists( $entity, $field )
     {
         return self::$pathResolver->isExists($entity, $field);
     }
 
+    /**
+     * Размер файл в человека понятном виде
+     * @param integer $size
+     * @return string
+     */
+    public function readableFilesize($size)
+    {
+        if( $size <= 0 ) {
+            return '0 KB';
+        }
+
+        if( $size === 1 ) {
+            return '1 byte';
+        }
+
+        $mod = 1024;
+        $units = array('bytes', 'KB', 'MB', 'GB', 'TB', 'PB');
+
+        for( $i = 0; $size > $mod && $i < count($units) - 1; ++$i ) {
+            $size /= $mod;
+        }
+
+        return round($size, 2) . ' ' . $units[$i];
+    }
 
     public function getName()
     {
