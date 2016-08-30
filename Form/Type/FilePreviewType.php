71efa43758875b4a7b82d306a7a88d4617d51063
Marina Bardiyan <marina.bard.96@gmail.com>
2016-08-30 12:34:21 +0300

    Добавлена поддержка Symfony 3

--- Form/Type/FilePreviewType.php
+++ Form/Type/FilePreviewType.php
@@ -13,6 +13,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class FilePreviewType extends AbstractType
 {
@@ -39,19 +40,12 @@ class FilePreviewType extends AbstractType
         }
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-
-        ));
-    }
-
     public function getParent()
     {
-        return 'file';
+        return FileType::class;
     }
 
-    public function getName()
+    public function getBlockPrefix()
     {
         return 'itm_file_preview';
     }
