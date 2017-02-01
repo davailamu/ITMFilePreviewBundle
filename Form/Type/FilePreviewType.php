4b062fb5f9bbd41ca374ac0e19314e6a867ca56f
Alexander Samusevich <aleksander.samusevich@itmhouse.com>
2017-02-01 11:46:59 +0300

    Fix use statement

--- Form/Type/FilePreviewType.php
+++ Form/Type/FilePreviewType.php
@@ -12,8 +12,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\FormInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpKernel\Kernel;
 
 class FilePreviewType extends AbstractType
 {
