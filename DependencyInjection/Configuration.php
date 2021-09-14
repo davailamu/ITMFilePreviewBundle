b2d00ac469bdb749020e13d41b60e7e2f53e0f52
Anna <davailamu@ya.ru>
2021-09-14 15:19:43 +0300

    added twig/extensions to dependencies, configuration upgraded

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('itm_file_preview');
+        $treeBuilder = new TreeBuilder('itm_file_preview');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
