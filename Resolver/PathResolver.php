ff53f82314d1b4e42bc4b72a94df87787c3155bb
Anna <davailamu@ya.ru>
2021-09-14 15:29:37 +0300

    root_dir changed to project_dir

--- Resolver/PathResolver.php
+++ Resolver/PathResolver.php
@@ -37,7 +37,7 @@ class PathResolver
         $path = str_replace("\\", '/', $config['upload_path'] . '/' . $entityClassName);
 
         if( !$relative ) {
-            $path = $this->container->getParameter('kernel.root_dir') . '/../' . $config['public_dir'] . '/' . $path;
+            $path = $this->container->getParameter('kernel.project_dir') . '/../' . $config['public_dir'] . '/' . $path;
         }
 
         return $path;
